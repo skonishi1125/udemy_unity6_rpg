922da6e0d4b46555b7dfb6b9b741802fc1d9ceea
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-10-24 23:16:26 +0900

    setting endless background

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,23 +4,38 @@ public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
 
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         float currentCameraPositionx = mainCamera.transform.position.x; // メインカメラの現在位置xの取得
         float distanceToMove = currentCameraPositionx - lastCameraPositionX;
         lastCameraPositionX = currentCameraPositionx;
 
+        float cameraLeftEdge = currentCameraPositionx - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionx + cameraHalfWidth;
+
         foreach(ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void CalculateImageLength()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }
--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -5,10 +5,33 @@ public class ParallaxLayer
 {
     [SerializeField] private Transform background;
     [SerializeField] private float parallaxMultiplier; // 視差倍率(カメラの何倍の速度で背景レイヤーを移動させるかの倍率)
+    [SerializeField] private float imageWidthOffset = 10;
+
+    private float imageFullWidth;
+    private float imageHalfWidth;
+
+    public void CalculateImageWidth()
+    {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
 
     public void Move(float distanceToMove)
     {
         //background.position = background.position + new Vector3(distanceToMove * parallaxMultiplier, 0);
         background.position += Vector3.right * (distanceToMove * parallaxMultiplier); // right: xだけ弄る場合の書き方 (x, 0,0)
+    }
+
+    public void LoopBackground(float cameraLeftEdge, float cameraRightEdge)
+    {
+        float imageRightEdge = (background.position.x + imageHalfWidth) - imageWidthOffset;
+        float imageLeftEdge = (background.position.x - imageHalfWidth) + imageWidthOffset;
+
+        // 背景を移動させて、動かしすぎた場合の調整処理
+        if (imageRightEdge < cameraLeftEdge)
+            background.position += Vector3.right * imageFullWidth;
+        else if (imageLeftEdge > cameraRightEdge)
+            background.position += Vector3.right * -imageFullWidth;
+
     }
 }
