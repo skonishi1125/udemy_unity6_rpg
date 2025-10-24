2d5d416abca030e5cc3131fafb2f5674881f211c
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-10-24 22:34:50 +0900

    added parallax layer

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float lastCameraPositionX;
+
+    [SerializeField] private ParallaxLayer[] backgroundLayers;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        float currentCameraPositionx = mainCamera.transform.position.x; // メインカメラの現在位置xの取得
+        float distanceToMove = currentCameraPositionx - lastCameraPositionX;
+        lastCameraPositionX = currentCameraPositionx;
+
+        foreach(ParallaxLayer layer in backgroundLayers)
+        {
+            layer.Move(distanceToMove);
+        }
+    }
+}
---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ParallaxLayer
+{
+    [SerializeField] private Transform background;
+    [SerializeField] private float parallaxMultiplier; // 視差倍率(カメラの何倍の速度で背景レイヤーを移動させるかの倍率)
+
+    public void Move(float distanceToMove)
+    {
+        //background.position = background.position + new Vector3(distanceToMove * parallaxMultiplier, 0);
+        background.position += Vector3.right * (distanceToMove * parallaxMultiplier); // right: xだけ弄る場合の書き方 (x, 0,0)
+    }
+}
