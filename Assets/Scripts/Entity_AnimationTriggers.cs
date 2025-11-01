20bd03ed8348f7269dffc3e67919a2b4f5888c77
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-01 12:06:25 +0900

    section6: attack detection

--- Assets/Scripts/Entity_AnimationTriggers.cs
+++ Assets/Scripts/Entity_AnimationTriggers.cs
@@ -3,13 +3,22 @@
 public class Entity_AnimationTriggers : MonoBehaviour
 {
     private Entity entity;
+    private Entity_Combat entityCombat;
 
     private void Awake()
     {
         entity = GetComponentInParent<Entity>();
+        entityCombat = GetComponentInParent<Entity_Combat>();
     }
+
     private void CurrentStateTrigger()
     {
         entity.CurrentStateAnimationTrigger();
     }
+
+    private void AttackTrigger()
+    {
+        entityCombat.PerformAttack();
+    }
+
 }
---
+++ Assets/Scripts/Entity_Combat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Entity_Combat : MonoBehaviour
+{
+
+    public Collider2D[] targetColliders;
+
+    // ターゲット検知
+    [Header("Target detection")]
+    [SerializeField] private Transform targetCheck;
+    [SerializeField] private float targetCheckRadius = 1;
+    [SerializeField] private LayerMask whatIsTarget;
+
+    public void PerformAttack()
+    {
+
+        foreach (var target in GetDetectedColliders())
+        {
+            Debug.Log("Attacking " + target.name);
+        }
+
+    }
+
+    private Collider2D[] GetDetectedColliders()
+    {
+        // Physicsチェック
+        // 該当のpositon, 半径以内に, 指定したレイヤーがあればそれを格納する
+        return Physics2D.OverlapCircleAll(targetCheck.position, targetCheckRadius, whatIsTarget);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(targetCheck.position, targetCheckRadius);
+    }
+
+}
