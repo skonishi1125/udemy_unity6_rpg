2f0c4a046fab0f44d63203cfa590131fc59bb95d
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-04 16:53:48 +0900

    cleanup codes

--- Assets/Scripts/Enemy/Enemy_VFX.cs
+++ Assets/Scripts/Enemy/Enemy_VFX.cs
@@ -5,5 +5,11 @@ public class Enemy_VFX : Entity_VFX
     [Header("Counter Attack Window")]
     [SerializeField] private GameObject attackAlert;
 
-    public void EnableAttackAlert(bool enable) => attackAlert.SetActive(enable);
+    public void EnableAttackAlert(bool enable)
+    {
+        if (attackAlert == null)
+            return;
+
+        attackAlert.SetActive(enable);
+    }
 }
