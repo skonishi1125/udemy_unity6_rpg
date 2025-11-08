46ede189110b0792f5c297bb0394e746b9f8ae6d
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-08 11:47:27 +0800

    add skill icons

---
+++ Assets/Scripts/UI/UI_TreeNode.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_TreeNode : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    [SerializeField] private Image skillIcon;
+    [SerializeField] private string lockedColorHex = "#9D9D9D";
+    private Color lastColor;
+    public bool isUnlocked;
+    public bool isLocked;
+
+    private void Awake()
+    {
+        UpdateIconColor(GetColorByHex(lockedColorHex)); // グレーみたいな色を被せて、アンロックっぽい見た目にする
+    }
+
+    private void Unlock()
+    {
+        isUnlocked = true;
+        UpdateIconColor(Color.white); // ロック解除っぽい見た目にする
+
+        // プレイヤーのSkillManagerにアクセス
+        // SkillManagerのSkillをアンロックする
+    }
+    private bool CanBeLocked()
+    {
+        if (isLocked || isUnlocked)
+            return false;
+
+        return true;
+    }
+
+    private void UpdateIconColor(Color color)
+    {
+        if (skillIcon == null)
+            return;
+
+        lastColor = skillIcon.color;
+
+        skillIcon.color = color;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (CanBeLocked())
+            Unlock();
+        else
+            Debug.Log("Cannot be Unlocked..");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (isUnlocked == false)
+            UpdateIconColor(Color.white * .9f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isUnlocked == false)
+            UpdateIconColor(lastColor);
+
+    }
+
+    private Color GetColorByHex(string hexNumber)
+    {
+        ColorUtility.TryParseHtmlString(hexNumber, out Color color);
+
+        return color;
+    }
+
+}
