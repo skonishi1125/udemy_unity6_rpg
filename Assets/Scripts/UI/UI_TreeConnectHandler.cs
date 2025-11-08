e1396ce9e7d2b0eb349c280f57de64eb59572df7
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-08 21:00:46 +0800

    adjust skill tree

---
+++ Assets/Scripts/UI/UI_TreeConnectHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+
+[Serializable]
+public class UI_TreeConnectDetails
+{
+    public UI_TreeConnectHandler childNode;
+    public NodeDirectionType direction;
+    [Range(100f, 350f)] public float length;
+}
+
+public class UI_TreeConnectHandler : MonoBehaviour
+{
+    private RectTransform rect;
+    [SerializeField] private UI_TreeConnectDetails[] connectionDetails;
+    [SerializeField] private UI_TreeConnection[] connections;
+
+    private void OnValidate()
+    {
+        if (rect == null)
+            rect = GetComponent<RectTransform>();
+
+        if (connectionDetails.Length != connections.Length)
+        {
+            Debug.Log("Amount of details should be same as amount of connections. - " + gameObject.name);
+            return;
+        }
+
+        UpdateConnection();
+    }
+
+    private void UpdateConnection()
+    {
+        for (int i = 0; i < connectionDetails.Length; i++)
+        {
+            var detail = connectionDetails[i];
+            var connection = connections[i];
+            Vector2 targetPosition = connection.GetConnectionPoint(rect);
+
+            connection.DirectConnection(detail.direction, detail.length);
+            detail.childNode.SetPosition(targetPosition);
+        }
+    }
+
+    public void SetPosition(Vector2 position) => rect.anchoredPosition = position;
+
+
+
+}
